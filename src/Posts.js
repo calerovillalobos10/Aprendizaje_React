cc99a604ad9648a16f62cf7a3e3ccec302bb25f8
Thomas <calerovillalobos10@gmail.com>
2022-10-13 16:25:06 -0600

    fetch Api

---
+++ src/Posts.js
@@ -0,0 +1,12 @@
+const Post = () => {
+    return <button onClick={()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+    }}>
+        Traer datos
+    </button>
+}
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Product from './Product';
 import { Button } from './Button';
 import { TaskCard } from './Task';
 import Saludar from './Saludar';
+import Post from './Posts';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
@@ -14,40 +15,5 @@ const handleChange = (event) => {
 
 root.render(<>
 
-    <TaskCard ready={true} />
-    <Saludar />
-    <Button text='Click me' name='Thomas' />
-    <input id='Hola' onChange={handleChange} onDoubleClick={()=>{console.log('doble click');}}/>
-
-    <form onSubmit={(event)=>{ 
-        event.preventDefault();
-        console.log('Enviado');
-        }}>
-        <h1>Registro de usuario</h1>
-        <button>Enviar</button>
-    </form>
-    {/* <Button text='Click me' name='Thomas'/>
-    <Button text='Pay'/>
-    <Button text='Go to'/> */}
-
-    {
-        /* <Greeting title='Hola Mundo' name='Joe' />
-        <Greeting title='Hola React' name='Bryan' />
-        <Greeting title='Hola JSX' />
-        <Greeting title='Hola JavaScript' />
-        <UserCard
-            name='Ryan Ray'
-            amount={3000}
-            married={true}
-            points={[99, 33.3, 22.2]} 
-            address={{ street: '123 Main Street', city: 'New York' }} 
-            greet = { () => { alert('Hola') } }/>
-        <UserCard
-            name='Joe Mcmillan'
-            amount={1000}
-            married={true}
-            points={[100, 20]} 
-            address={{ street: 'av some 123', city: 'New York' }} />
-        <Product /> */
-    }
+    <Post />
 </>);
