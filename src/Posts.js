9d21a0eda90e83964b9d8fedbd130183c4a2dba1
Thomas <calerovillalobos10@gmail.com>
2022-10-14 09:15:48 -0600

    Implementación de librería de tercero

--- src/Posts.js
+++ src/Posts.js
@@ -1,10 +1,13 @@
+import { VscGlobe } from 'react-icons/vsc';
+
 const Post = () => {
-    return <button onClick={()=>{
+    return <button onClick={() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.log(error));
     }}>
+        <VscGlobe />
         Traer datos
     </button>
 }
