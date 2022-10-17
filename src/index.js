3e9663aded0ed7b9dd8b324af42a56c12ec82ec2
Thomas <calerovillalobos10@gmail.com>
2022-10-17 11:24:36 -0600

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom/client';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
@@ -7,15 +7,29 @@ const Counter = () => {
 
     // A esto se le conoce como un estado
     //      Variable, Función
-    const [ mensaje, setMensaje ] = useState('');//Se debe poner dentro del useState el valor con el que se quiere inicializar
+    const [mensaje, setMensaje] = useState('');//Se debe poner dentro del useState el valor con el que se quiere inicializar
+    const [counter, setCounter] = useState(0);
+    // El use efect siempre se ejecuta cuando hay un cambio en el componente
+    // Espera por parámetro una función
+    useEffect(() => {
+        console.log('Render');
+    }, [counter]); // El arreglo vacío funciona para que se ejecute una sola vez
+    // El  [counter] es una dependencia
 
     return (
         <div>
-            <input onChange={ event => setMensaje(event.target.value)}/>
-            <button onClick={ () => {
+            <input onChange={event => setMensaje(event.target.value)} />
+            <button onClick={() => {
                 alert('El mensaje es: ' + mensaje);
             }}>
                 Save
+            </button>
+
+            <hr />
+
+            <h1>Counter: {counter}</h1>
+            <button onClick={() => { setCounter(counter + 1) }}>
+                Incrementar
             </button>
         </div>
     );
